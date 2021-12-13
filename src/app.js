b74be3a1854ba26ea162b2b672d1f4c00b5a7ed1
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-12-13 17:34:15 -0300

    Find non terminated contracts for an user

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
 const FindContractService = require('./services/findContractService')
+const FindNonTerminatedContractService = require('./services/findNonTerminatedContractsService')
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,11 +13,21 @@ app.set('models', sequelize.models)
 /**
  * @returns contract by id and profile
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
+app.get('/contracts/:id', getProfile, async (req, res) =>{
     const {id} = req.params
     const contract = await FindContractService.call({id, profile_id: req.profile.id })
-
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
+
+/**
+ * @returns non terminated contracts
+ */
+app.get('/contracts', getProfile, async (req, res) =>{
+    const {id} = req.params
+    const contract = await FindNonTerminatedContractService.call({profile_id: req.profile.id })
+    if(!contract) return res.status(404).end()
+    res.json(contract)
+})
+
 module.exports = app;
